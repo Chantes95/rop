7365f4b5992ceb76892ba37b6bbbd97f23cfa83c
Chantes95 <152847602+Chantes95@users.noreply.github.com>
2024-09-10 18:21:32 -0600

    Update coloresox.js

--- js/coloresox.js
+++ js/coloresox.js
@@ -1,31 +1,30 @@
 function dragElement(elmnt) {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    
-    // Eventos para dispositivos con mouse
-    elmnt.onmousedown = dragMouseDown;
 
-    // Eventos para dispositivos táctiles
-    elmnt.ontouchstart = dragTouchStart;
+    // Asignar eventos para el mouse
+    elmnt.addEventListener('mousedown', dragMouseDown);
+    // Asignar eventos para touch en móviles
+    elmnt.addEventListener('touchstart', dragTouchStart, { passive: false });
 
-    // Funciones para el mouse
+    // Funciones para eventos del mouse
     function dragMouseDown(e) {
         e.preventDefault();
         pos3 = e.clientX;
         pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
+        document.addEventListener('mouseup', closeDragElement);
+        document.addEventListener('mousemove', elementDrag);
     }
 
-    // Funciones para el toque táctil
+    // Funciones para eventos táctiles
     function dragTouchStart(e) {
-        e.preventDefault();
+        e.preventDefault(); // Previene el comportamiento predeterminado de desplazamiento en móviles
         pos3 = e.touches[0].clientX;
         pos4 = e.touches[0].clientY;
-        document.ontouchend = closeDragElement;
-        document.ontouchmove = elementTouchDrag;
+        document.addEventListener('touchend', closeDragElement, { passive: false });
+        document.addEventListener('touchmove', elementTouchDrag, { passive: false });
     }
 
-    // Movimiento para el mouse
+    // Movimiento con el mouse
     function elementDrag(e) {
         e.preventDefault();
         pos1 = pos3 - e.clientX;
@@ -36,9 +35,9 @@ function dragElement(elmnt) {
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
     }
 
-    // Movimiento para el toque táctil
+    // Movimiento con el toque táctil
     function elementTouchDrag(e) {
-        e.preventDefault();
+        e.preventDefault(); // Previene el comportamiento predeterminado de desplazamiento en móviles
         pos1 = pos3 - e.touches[0].clientX;
         pos2 = pos4 - e.touches[0].clientY;
         pos3 = e.touches[0].clientX;
@@ -47,11 +46,12 @@ function dragElement(elmnt) {
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
     }
 
-    // Finalizar el arrastre para ambos (mouse y toque)
+    // Finaliza el arrastre para ambos (mouse y toque táctil)
     function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
-        document.ontouchend = null;
-        document.ontouchmove = null;
+        document.removeEventListener('mouseup', closeDragElement);
+        document.removeEventListener('mousemove', elementDrag);
+        document.removeEventListener('touchend', closeDragElement);
+        document.removeEventListener('touchmove', elementTouchDrag);
     }
 }
+
